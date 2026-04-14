60a65524f4dfe58e1470e7f430d0e1377664a716
Satgur456 <162884652+Satgur456@users.noreply.github.com>
2026-04-14 21:05:27 +0530

    Initial commit for Next.js landing page project

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Welcome to My Landing Page</title>
+      </Head>
+      <main>
+        <h1>Welcome to My Landing Page</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
